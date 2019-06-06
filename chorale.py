20cee0820404a5615c93b2d6cb255524274dd060
lorenzomammana <lorenzom96@hotmail.it>
2019-06-06 17:43:20 +0200

    Initial reimplementation

---
+++ chorale.py
@@ -0,0 +1,254 @@
+import re
+
+
+class Chorale:
+    tabbeddata = 1
+    columnheadings = "PHRASE\tTAKT\tSOPRAN\tALT\tTENOR\tBASS\tHARMONIK"
+    columns = 1
+    columnbreaks = []
+
+    notepitches = {
+        'C': 0,
+        'C#': 1, 'Db': 1, 'Csharp': 1, 'Dflat': 1,
+        'D': 2,
+        'D#': 3, 'Eb': 3, 'Dsharp': 3, 'Eflat': 3,
+        'E': 4,
+        'F': 5, 'E#': 5,
+        'F#': 6, 'Gb': 6, 'Fsharp': 6, 'Gflat': 6,
+        'G': 7,
+        'G#': 8, 'Ab': 8, 'Gsharp': 8, 'Aflat': 8,
+        'A': 9,
+        'A#': 10, 'B': 10, 'Hb': 10,
+        'H': 11, 'B#': 11,
+        'H#': 12,
+    }
+
+    notesymbols = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'B', 'H', 'P']
+
+    BEFORE = ["0", "0", "P", "P", "P", "P", "0", "P,P,P,P", "0", "P:P:P:P",
+              "P;P;P;P", "P,P,P,P", "P,P,P,P", "P,P,P,P", "P,P,P,P"]
+
+    AFTER = ["0", "0", "END", "END", "END", "END", "0", "P,P,P,P", "0",
+             "P:P:P:P", "P;P;P;P", "P,P,P,P", "P,P,P,P", "P,P,P,P", "P,P,P,P"]
+
+    # write out the header lines of a chorale data file
+    def writeheaders(self, choralname, stimmen, tonart, takt, tempo, message):
+        print(choralname)
+        print(stimmen)
+        print(tonart)
+        print(takt)
+        print(tempo)
+        print(message)
+        print(self.columnheadings)
+
+    def readheaders(self, file):
+        choralname = ""
+        stimmen = ""
+        tonart = ""
+        takt = ""
+        tempo = ""
+        message = ""
+
+        for line in file:
+            if line == "\n":
+                break
+
+            if "Choralname" in line:
+                choralname = line.split('=')[1].split("\n")[0][1:]
+            if "Stimmen" in line:
+                stimmen = line.split('=')[1].split("\n")[0][1:]
+            if "Tonart" in line:
+                tonart = line.split('=')[1].split("\n")[0][1:]
+            if "Takt" in line:
+                takt = line.split('=')[1].split("\n")[0][1:]
+            if "Tempo" in line:
+                tempo = line.split('=')[1].split("\n")[0][1:]
+            if "Notentextausgabe" in line:
+                message = line
+
+        headers = file.readline()
+        self.columnheadings = headers
+
+        return [choralname, stimmen, tonart, takt, tempo, message]
+
+    # convert a note representation into a numerical value
+    def notepitch(self, note):
+
+        if "P" in note:
+            return -1
+
+        match = re.match('^\-*([ABCDEFGH#b]+)[ _]*([-\d]+)', note)
+        symbol = match.group(1)
+        octave = int(match.group(2))
+
+        noteval = self.notepitches[symbol]
+        return noteval + (octave + 4) * 12
+
+    def key2pitch(self, tonart):
+        tone = tonart.split("-")[0]
+        tone = tone + " -4"
+        return self.notepitch(tone)
+
+    def notesymbol(self, note):  # convert a numerical note value to a textual representation
+        noteval = note % 12
+        octave = ((note - noteval) / 12) - 4
+        symbol = self.notesymbols[noteval]
+
+        if len(symbol) == 1:
+            symbol += " "
+
+        symbol += str(int(octave))
+
+        return symbol
+
+    def transposerow(self, keypitch, row):
+
+        for i in range(2, 6):
+            if row[i] != "" and "P" not in row[i]:
+                if row[i][0] == "-":
+                    row[i] = "-" + self.notesymbol(self.notepitch(row[i]) - keypitch)
+                else:
+                    row[i] = "" + self.notesymbol(self.notepitch(row[i]) - keypitch)
+
+        return row
+
+    def readchorale(self, filename, notranspose=False):
+        f = open(filename, 'r')
+
+        headers = self.readheaders(f)
+
+        [choralname, stimmen, tonart, takt, tempo, message] = headers
+
+        keypitch = self.key2pitch(tonart)
+
+        # event we're 'in' (so nothing to start with)
+        within = self.BEFORE
+
+        lines = []
+
+        f.readline()
+
+        cont = 0
+        for line in f:
+            line = line.split("\n")[0]
+
+            row = line.split("\t")
+
+            if row == ['']:
+                row = ['', '', '', '', '', '', '']
+            elif not notranspose:
+                # transpose to C major/C minor
+                self.transposerow(keypitch, row)
+
+            for i in range(0, 7):
+                if row[i] == '':
+                    if within[i][0] != "-":
+                        row[i] = "-" + within[i]
+                    else:
+                        row[i] = within[i]
+                else:
+                    row[i] = row[i].replace(" ", "_")  # replace any spaces in tokens with underscores
+                    within[i] = row[i]
+
+            intervals = ["0", "0", "0", "0"]
+            bass = None
+
+            for i in range(5, 1, -1):
+                if "P" in row[i] or row[i] == "END":
+                    intervals[i - 2] = row[i]
+                else:
+                    if bass is None:
+                        bass = self.notepitch(row[i])
+
+                    intervals[i - 2] = str(self.notepitch(row[i]) - bass)
+                    if row[i][0] == "-":
+                            intervals[i - 2] = "-" + intervals[i - 2]
+
+            row.append(":".join(intervals))
+            lines.append(row)
+
+            cont += 1
+
+        f.close()
+
+        if "/" not in lines[0][1]:
+            maxbeat = 0
+            beat = -1
+
+            for i in range(0, len(lines)):
+
+                row = lines[i]
+                if i % 4 == 0:
+                    beat += 1
+
+                if "-" not in row[1] and row[1] != "0":
+                    beat = 1
+
+                if i % 4 != 0:
+                    row[1] = "-" + str(beat)
+                else:
+                    row[1] = str(beat)
+
+                if beat > maxbeat:
+                    maxbeat = beat
+
+                lines[i] = row
+
+            for i in range(0, len(lines)):
+                lines[i][1] = lines[i][1] + "/" + str(maxbeat)
+
+        return [headers, lines]
+
+    def beatrows(self, lines):
+
+        linesB = []
+
+        for i in range(0, len(lines), +4):
+            row = lines[i]
+            ornamentation = []
+            tornamentation = []  # transposed relative to initial notes
+
+            for j in range(0, 4):
+                sequence = []
+                tsequence = []
+
+                sequence = [row[j + 2], lines[i + 1][j + 2], lines[i + 2][j + 2], lines[i + 3][j + 2]]
+                ornamentation.append(",".join(sequence))
+
+                firstnote = None
+
+                for note in sequence:
+                    firstnote = self.notepitch(note)
+
+                    if "-" not in note and "P" not in note and note != "" and note != "END":
+                        tsequence.append(str(self.notepitch(note) - firstnote))
+                    else:
+                        tsequence.append("-")
+
+                tornamentation.append(",".join(tsequence))
+
+            row = row[0:7] + ["/".join(ornamentation)] + [row[7]]
+            row.append(";".join(tornamentation))
+            row.append(ornamentation[0])
+            row.append(tornamentation[1])
+            row.append(tornamentation[2])
+            row.append(tornamentation[3])
+
+            if i + 4 > len(lines) - 1:
+                row = row[0:7] + ["END,END,END,END"] + row[7:len(row)]
+            else:
+                nextbeatrow = lines[i + 4]
+                intervals = []  # to next notes
+                for j in range(0, 4):
+                    # / - * P /
+                    if "P" not in nextbeatrow[j + 2] and "P" not in row[j + 2]:
+                        intervals.append(str(self.notepitch(nextbeatrow[j + 2]) - self.notepitch(row[j + 2])) + ">" +
+                                         str(self.notepitch(row[j + 2]) % 12))
+                    else:
+                        intervals.append("P")
+
+                row = row[0:7] + [",".join(intervals)] + row[7:len(row)]  # intervals to next beat's notes
+
+            linesB.append(row)
+
+        return linesB
---
+++ hmm-data.py
@@ -0,0 +1,140 @@
+# if (#ARGV != 4)
+# {
+# 	die "syntax: hmm-data.pl MODELNAME HIDDEN VISIBLE TRAIN TEST")
+# }
+#
+# my choraledir = ENV{HARMONYDIR})
+# my modeldir = ENV{HARMONYOUTPUTDIR})
+
+import sys
+from chorale import Chorale
+import os
+
+
+def internal(arg):
+    return {
+        'PHRASE': 'column[0]',
+        'TAKT': 'column[1]',
+        'SOPRAN': 'column[2]',
+        'ALT': 'column[3]',
+        'TENOR': 'column[4]',
+        'BASS': 'column[5]',
+        'HARMONIK': 'column[6]',
+        'chordtransposed': 'column[9]'
+    }[arg]
+
+
+def readentries(filename):
+    entries = []
+
+    f = open(filename, "r")
+
+    for chor in f:
+        entries.append("bch" + ('%03d' % int(chor.split("\n")[0])) + ".txt")
+
+    f.close()
+
+    return entries
+
+
+choraledir = ""
+modeldir = ""
+
+inputdir = choraledir + "music/"
+datasetdir = choraledir + "datasets/"
+name = sys.argv[1]
+
+if "/" in sys.argv[2]:
+    splits = sys.argv[2].split('/')
+    hidden = internal(splits[0]) + '/' + internal(splits[1])
+else:
+    hidden = internal(sys.argv[1])
+
+visible = internal(sys.argv[3])
+train = sys.argv[4]
+test = sys.argv[5]
+
+unseenmarker = "(unknown)"
+hiddensymbols = [unseenmarker]
+hiddensymbolseen = {unseenmarker: 0}
+visiblesymbols = [unseenmarker]
+visiblesymbolseen = {unseenmarker: 0}
+
+modeldir += "model-" + name + "/"
+
+if not os.path.exists(modeldir):
+    os.mkdir(modeldir)
+    os.mkdir(modeldir + "input")
+    os.mkdir(modeldir + "viterbi")
+    os.mkdir(modeldir + "sampled")
+
+#  perl -I chorale-perl chorale-perl/hmm-data.pl chords-dur chordtransposed/HARMONIK SOPRAN train_dur test_dur
+
+# carica due array con i nomi dei file
+trainfiles = readentries(datasetdir + train)
+testfiles = readentries(datasetdir + test)
+
+
+def processfile(file):
+
+    print("Processing " + file)
+    [headers, lines] = choraleManager.readchorale("music/" + file)
+
+    linesB = choraleManager.beatrows(lines)
+
+    linesB.append(choraleManager.AFTER)
+
+    outputfile = open(modeldir + "input/" + file, "w+")
+
+    for line in linesB:
+
+        column = line
+        if "/" in hidden:
+            hiddensymbol = eval(hidden.split("/")[0]) + "/" + eval(hidden.split("/")[1])
+        else:
+            hiddensymbol = eval(hidden)
+
+        visiblesymbol = eval(visible)
+
+        try:
+            hiddensymbols.index(hiddensymbol)
+        except ValueError:
+            hiddensymbols.append(hiddensymbol)
+
+        try:
+            visiblesymbols.index(visiblesymbol)
+        except ValueError:
+            visiblesymbols.append(visiblesymbol)
+
+        outputfile.write(str(hiddensymbols.index(hiddensymbol)) + "\t" +
+                         str(visiblesymbols.index(visiblesymbol)) + "\n")
+
+    outputfile.close()
+
+
+if __name__ == '__main__':
+    choraleManager = Chorale()
+
+    for file in trainfiles:
+        processfile(file)
+
+    for file in testfiles:
+        processfile(file)
+
+    f = open(modeldir + "SYMBOLS_HIDDEN", "w+")
+    f.write("\n".join(hiddensymbols))
+    f.close()
+
+    f = open(modeldir + "SYMBOLS_VISIBLE", "w+")
+    f.write("\n".join(visiblesymbols))
+    f.close()
+
+    f = open(modeldir + "PARAMETERS", "w+")
+    f.write("Name: " + name + "\n")
+    f.write("Hidden: " + sys.argv[2] + "\n")
+    f.write("Visible: " + sys.argv[3] + "\n\n")
+    f.write("Hidden states: " + str(len(hiddensymbols)) + "\n")
+    f.write("Visible states: " + str(len(visiblesymbols)) + "\n")
+    f.write("Training data: " + train + "\n")
+    f.write("Test data: " + test + "\n")
+    f.close()
